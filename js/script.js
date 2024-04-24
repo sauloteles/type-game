d293ae6bf6a3a6d9355d6690d90c3fee41a07bd9
sauloteles <saulot3l35@gmail.com>
2024-04-23 22:06:09 -0300

    add funcionalidades

--- js/script.js
+++ js/script.js
@@ -1,50 +1,45 @@
 const canvas = document.querySelector('#canvas')
 const ctx = canvas.getContext('2d');
-console.log(ctx)
-const quadrado = ctx;
 
 let palavraCount = 0;
-const texto1 = ctx;
-const texto2 = ctx;
 let listWord = ['pink','yellow','black','white','green'] 
-let mover = -listWord[palavraCount].length-50;
+let mover = 0;
 let run = false;
-
 function palavraAtual(listWord){
     if(mover == canvas.width+listWord[palavraCount].length || listWord[palavraCount] == palavraEscrita){
         ++palavraCount;
         if(palavraCount == listWord.length){
             palavraCount = 0
         }
-        mover = -listWord[palavraCount].length-50;
+        mover = 0;
         palavraEscrita = ''
         
     }
     return listWord[palavraCount];
 }
-function Desenhar() {
+function Desenhar() {   
     ctx.beginPath()
     ctx.font = "24px serif";
     ctx.fillStyle = 'white'
     ctx.fillText(palavraAtual(listWord), mover, canvas.height/2);
-    
-    ctx.beginPath()
-    ctx.font = "24px serif";
-    ctx.fillStyle = 'white'
-    ctx.fillText(palavraEscrita, 400, 390);
 
+    corrigirCaracter(palavraAtual(listWord),mover);
     ++mover;
 }
 function menu(){
     ctx.beginPath()
-    ctx.font = "32px serif";
+    ctx.font = "24px serif";
     ctx.fillStyle = 'white'
-    ctx.fillText('start', canvas.width/2, canvas.height/2);
-    ctx.closePath()
+    ctx.textBaseline = 'middle'
+    ctx.fillText('start',450,canvas.height/2);
+
+    corrigirCaracter('start',450)
+
     if(palavraEscrita == 'start'){
         run = true
         palavraEscrita = ''
     }
+
 }
 function LimparTela() {
     ctx.fillStyle = "black";
@@ -56,15 +51,41 @@ function Iniciar() {
 }
 function Atualizar() {
     LimparTela();
+    mostrarInput()
     if(run){
         Desenhar();
     }else{
         menu()
     }
 }
+
+function corrigirCaracter(palavra,posX,posY){
+    ctx.beginPath()    
+    ctx.font = "24px serif";
+    if(palavraEscrita.length){
+        for(let i = 0; i < palavraEscrita.length;++i){
+            if(palavra.charAt(i) == palavraEscrita.charAt(i)){
+                ctx.fillStyle = 'green';   
+                ctx.fillText(palavraEscrita, posX, canvas.height/2);
+            }else{
+                ctx.fillStyle = 'red';
+                ctx.fillText(palavraEscrita, posX, canvas.height/2);
+                break;
+            }
+            
+        }
+    }
+
+}
 Iniciar()
 
 let palavraEscrita = ''
+function mostrarInput(){
+    ctx.beginPath()
+    ctx.font = "24px serif";
+    ctx.fillStyle = 'white';
+    ctx.fillText(palavraEscrita, 400, 390);
+}
 
 document.addEventListener("keydown",(event)=>{
     console.log(event.key)
@@ -76,7 +97,7 @@ document.addEventListener("keydown",(event)=>{
         console.log(palavraEscrita)
     }if(' ' == event.key){
         palavraEscrita += ' ';
+    }if('1' == event.key){
+        palavraEscrita = '';
     }
-
 })
-
